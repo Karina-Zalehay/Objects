b4e21b6fc88073fb877712e14ff286d1248ed5c8
kzaliahai <kzaliahai@evolution.com>
2022-10-27 17:27:25 +0300

    Working with object properties

---
+++ assets/scripts/objects.js
@@ -0,0 +1,31 @@
+const movieList = document.getElementById('movie-list');
+movieList.style['backgroundColor'] = 'red';
+movieList.style.display = 'block';
+
+const userChosenKeyName = 'level';
+
+let person = {
+    'first name': 'Max',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    [userChosenKeyName]: 'ggg',
+    greet: function() {
+        alert('Hi there!')
+    },
+    1.5: 'hello'
+};
+
+const keyName = userChosenKeyName;
+
+person.isAdmin = true; //add
+// delete person.age;
+person.age = null;
+// console.log(person['first name']);
+console.log(person[1.5]);
+console.log(person[keyName]);
+
+// const propKey = 'field 12';
+// const person = {
+//     [propKey]: 'Max'
+// };
+// console.log(person['field 12']);
